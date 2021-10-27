82c816615caea315a10df42b0e72832e49af071b
Fabipacheco <90794824+Fabipacheco@users.noreply.github.com>
2021-10-26 21:35:07 -0300

    Create Somando vetores.py

---
+++ Somando vetores.py
@@ -0,0 +1,11 @@
+Vetor1 = [ ]
+Vetor2 = [ ]
+Soma = [ ] 
+For n in range(0,10):
+     Num1 = int(input(“Informe o valor para o vetor 1:”))
+     Vetor1.append(num1)
+     Num2 = int(input(“Informe o valor para o vetor 2:”))
+     Vetor2.append(num2)
+     Soma.append(num1 + num2)
+For n in soma:
+     Print(n)
