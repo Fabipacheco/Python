1dea27fcda7411a91c22cac3a70b281a2fbb8041
Fabipacheco <90794824+Fabipacheco@users.noreply.github.com>
2021-10-26 21:33:33 -0300

    Create Estoque médio.py

---
+++ Estoque médio.py
@@ -0,0 +1,6 @@
+Quantidade_minima = int (input (“Informe a quantidade mínima”))
+Quatidade_maxima = int (input (“Informe a quantidade máxima”))
+
+Estoque_medio = quantidade_minima + quantidade_maxima / 2
+
+Print (“O estoque médio é {0}”.format(estoque_medio))
